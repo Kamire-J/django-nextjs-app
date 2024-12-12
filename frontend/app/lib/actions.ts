f0fce891fc061f8e7dff2b5ff792f4cddd375396
Jack Kamire <jacmire@gmail.com>
2024-12-12 18:58:53 +0900

    code changes

--- frontend/app/lib/actions.ts
+++ frontend/app/lib/actions.ts
@@ -7,7 +7,7 @@ export async function handleRefresh() {
 
     const refreshToken = await getRefreshToken();
 
-    const token = await fetch('http://localhost:8000/api/auth/token/refresh/', {
+    const token = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/auth/token/refresh/`, {
         method: 'POST',
         body: JSON.stringify({
             refresh: refreshToken
